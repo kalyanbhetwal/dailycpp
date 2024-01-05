6c8341318aa0a4b497e76680db52eb1545f5ae64
kalyanbhetwal <kalyanbtl@gmail.com>
2024-01-04 22:53:00 -0700

    type def of function

--- day2_fp.cpp
+++ day2_fp.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+typedef int(*PFIntegeroperations)(int, int);
 using namespace std;
 int add(int a, int b){
     return a + b;
@@ -10,7 +11,8 @@ int multiply(int a, int b){
 int main(void){
 
     int  a = 5, b = 6;
-    int(*fp)(int, int); 
+    //int(*fp)(int, int); 
+    PFIntegeroperations fp;
     fp = add;
     cout<< "add " << fp(a,b)<<'\n';
     cout << "multiply "<<multiply(a,b)<<'\n';
