ca5e3ec005413b432928f39ec017a84709ab5eb9
kalyanbhetwal <kalyanbtl@gmail.com>
2024-01-04 22:29:28 -0700

    inbetween examples of pointer issues

---
+++ day2_arrays.cpp
@@ -0,0 +1,7 @@
+
+#include <iostream>
+using namespace std;
+
+int main(void){
+   std::array<int, 5> = {1,2,3}
+}
---
+++ day2_nullptr_pitfall.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+using namespace std;
+
+
+
+int main(void){
+     //int *px = nullptr; // nullptr derefence
+     //*px = 32;
+    // int *px = new int[5000]; // not deleting the allocated memory 
+    // g++ -g -fsanitize=address -std=c++17 day2_nullptr_pitfall.cpp 
+
+    return 0;
+     //
+}
