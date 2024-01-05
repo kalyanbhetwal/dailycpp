6f115dbffeaf0a03c0b2b8205310cf6104f59905
kalyanbhetwal <kalyanbtl@gmail.com>
2024-01-05 07:48:47 -0700

    day2 lvalue rvalue

---
+++ day2_lvalue.cpp
@@ -0,0 +1,12 @@
+
+int main(void){
+    // lvalue (location value) something with memory location
+    // rvalue (on the right side /an assignment)
+    // lvalue_reference 
+    // const int& a= 10;
+
+    //rvalue_reference
+    
+    return 0;
+
+}
