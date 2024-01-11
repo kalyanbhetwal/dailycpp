400ac6957abe5dcb9b82b124190afefe8bd56276
kalyanbhetwal <kalyanbtl@gmail.com>
2024-01-10 21:50:57 -0700

    initializer

---
+++ day7_intializerlists.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+//performance between initializer and assignment
+class UDT{
+    public:
+    UDT(): x(1.0f),y(2.0f),z(2.0f){
+
+    }
+    float x,y,z;
+
+};
+
+using namespace std;
+int main(void){
+    
+}
