33059fad21f688b961f966db23cead21a2ec8410
kalyanbhetwal <kalyanbtl@gmail.com>
2024-01-12 23:15:19 -0700

    inheritance

--- day10_inheritance.cpp
+++ day10_inheritance.cpp
@@ -2,12 +2,28 @@
 #include <iostream>
 using namespace std;
 class Dog{
+    public:
+    Dog(){
+
+    }
+    void Bark(){
+        std::cout << "bark!"<<std::endl;
+    }
+    void Walk(){
+        x+=1;
+        y+=1;
+        z+=1;
+    }
+    float x,y,z;
+
 
 };
 class Gloden : public Dog{
-    
-}
+
+};
 
 int main(void){
-    
+    Gloden dog1;
+    dog1.Bark();
+    dog1.Walk();
 }
