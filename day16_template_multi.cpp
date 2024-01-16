5c2ab2455e763752538db2716a17dcfd3455cdf5
kalyanbhetwal <kalyanbtl@gmail.com>
2024-01-16 11:32:38 -0700

    function with multiple template input

---
+++ day16_template_multi.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+template <typename T1, typename T2>
+void add(T1 i1, T2 i2){
+    cout << i1 + i2<<endl;
+}
+int main(){
+    add(1,3);
+    add<float,float>(1.1,3);
+
+}
