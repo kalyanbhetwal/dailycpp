6436b1ce83cdc0d73e4ef67060f319952cca388b
kalyanbhetwal <kalyanbtl@gmail.com>
2024-01-17 10:19:19 -0700

    stl notes

---
+++ stl.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+//Containers: "Data Structures"
+//-> A collections of <elements>
+//Algorithms: "Operations on elements"
+//Iterators: Abstraction for accessing our containers in a sequential or 
+//random access manner. Different ways to maneuver through containers.
+// container <---iterator---> Algorithm.
+int main(){
+    
+}
