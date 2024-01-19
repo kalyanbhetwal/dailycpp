567ee000df66018dadd637ea6ae8a0b8a5c24766
kalyanbhetwal <kalyanbtl@gmail.com>
2024-01-18 22:01:13 -0700

    deque example update

--- day18_deque.cpp
+++ day18_deque.cpp
@@ -5,6 +5,13 @@ using namespace std;
 //deque is similar to a vector, but limited in 
 //insertion/removal to front and back.
 int main(){
-    std::deque<int>{1,2,4,5};
-
+    std::deque<int>test{1,2,4,5};
+    test.push_front(7);
+    test.push_front(8);
+    test.push_back(7);
+    test.push_front(8);
+    for(const auto& a: test){
+        cout << a <<endl;
+    }
+ 
 }
